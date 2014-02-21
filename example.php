5901f8f4ed282e4a9087ca89e48444ab5a0b77eb
fontai_d <fontai_d@epitech.eu>
2014-02-21 11:32:44 +0100

    Commantaire enlevé dans l'example.

--- example.php
+++ example.php
@@ -13,23 +13,7 @@ $time->calcTime_start();
 $imap = new Imap('./', '<email@gmail.com>', '<mot_de_passe>');
 
 $mails = $imap->getUnreadMessages();
-//$nbunread = $imap->getNumUnreadMessages();
-/*
-$i = -1;
-while (++$i < $nbunread)
-{
-    $from = $mails[$i]->from[0]->mailbox.'@'.$mails[$i]->from[0]->host;
-    $subject = $imap->getHeaderDecode($mails[$i]->subject, 'text');
-    $message = $imap->getBodyFullDecode($mails[$i]->Msgno,
-                $imap->getSimpleBodyMessage($mails[$i]->Msgno));
-    
-    echo $from.'<br />';
-    echo $subject.'<br />';
-    echo $message.'<br />';
-    
-    $imap->setSeenMessage($mails[$i]->Msgno);
-}
-*/
+
 foreach ($mails as $data)
 {
     $from = $data->from[0]->mailbox.'@'.$data->from[0]->host;
